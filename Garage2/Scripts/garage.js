c8c5c8b49b77d96d9f6998ecaae74f380f7f4e94
Lennart0 <lennart0@hotmail.se>
2016-05-18 10:56:33 +0200

    Changed ParkingLot column in search view (etc...)

--- Garage2/Scripts/garage.js
+++ Garage2/Scripts/garage.js
@@ -1,17 +1,16 @@
 ﻿function togglePark(modelId) {
-    $form = $(this);
     options = {
         url: "/Vehicles/TogglePark/" + modelId
     }
 
     $.ajax(options).done(function (data) {
-        var cbIx = data.lastIndexOf("<td");
-        if (cbIx > 0) {
-            var tpSubData = data.substring(0, cbIx),
-                pSubData = data.substring(cbIx);
-
-            $('#timeParkedTableData' + modelId).replaceWith(tpSubData);
-            $('#parkToggleForm' + modelId).replaceWith(pSubData);
+        var cbIx = data.indexOf("<tr");
+        if (cbIx >= 0) {
+            $('#vehicleTableRow' + modelId).replaceWith(data);
+            //var tpSubData = data.substring(0, cbIx),
+            //    pSubData = data.substring(cbIx);
+            //$('#timeParkedTableData' + modelId).replaceWith(tpSubData);
+            //$('#parkToggleForm' + modelId).replaceWith(pSubData);
             //$($form.attr("id")).replaceWith(pSubData);
         }
         else

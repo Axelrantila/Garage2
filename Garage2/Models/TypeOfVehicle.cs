e3a8bb9cf27d73d039bc00ac2adbddf9d1a91d18
Lennart0 <lennart0@hotmail.se>
2016-05-24 14:48:24 +0200

    Added VehicleTypeId to Vehicle and made temorary new scaffolding.

--- Garage2/Models/TypeOfVehicle.cs
+++ Garage2/Models/TypeOfVehicle.cs
@@ -9,5 +9,10 @@ namespace Garage2.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
